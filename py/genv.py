d54e2e847094a0d184194a0b9b7e9b13bce11874
Raz <raz.rotenberg@gmail.com>
2022-09-06 21:44:43 +0300

    Protecting json files with flocks

--- py/genv.py
+++ py/genv.py
@@ -1,20 +1,13 @@
+from contextlib import contextmanager
 from datetime import datetime
 import errno
+import fcntl
 import json
 import os
 from pathlib import Path
-from typing import Optional, Union
+from typing import Callable, Dict, Union
 
 DATETIME_FMT = '%d/%m/%Y %H:%M:%S'
-
-def env(name: str, default: str=None) -> Optional[str]:
-    return os.environ.get(f'GENV_{name}', default)
-
-def exists(filename: str) -> bool:
-    return os.path.exists(path(filename))
-
-def path(filename: str) -> str:
-    return os.path.join(env('TMPDIR', '/var/tmp/genv'), filename)
 
 def poll(pid: int) -> bool:
     try:
@@ -28,22 +21,6 @@ def poll(pid: int) -> bool:
             raise
     else:
         return True
-
-def read(filename: str) -> dict:
-    with open(path(filename)) as f:
-        return json.load(f)
-
-def save(filename: str, o: dict) -> None:
-    path_ = path(filename)
-
-    umask = os.umask(0)
-    try:
-        Path(path_).parent.mkdir(parents=True, exist_ok=True, mode=0o777)
-
-        with open(path_, 'w', opener=lambda path, flags: os.open(path, flags, 0o666)) as f:
-            json.dump(o, f, indent=4)
-    finally:
-        os.umask(umask)
 
 def time_since(dt: Union[str, datetime]) -> str:
     if isinstance(dt, str):
@@ -68,3 +45,50 @@ def time_since(dt: Union[str, datetime]) -> str:
         unit = f'{unit}s'
 
     return f'{value} {unit} ago'
+
+@contextmanager
+def Umask(value: int=0):
+    prev = os.umask(value)
+    try:
+        yield
+    finally:
+        os.umask(prev)
+
+class Flock:
+    def __init__(self, path: str, mode: int):
+        self._path = path
+        self._mode = mode
+
+    def __enter__(self):
+        self._fd = os.open(self._path, os.O_RDWR | os.O_CREAT | os.O_TRUNC, self._mode)
+
+        try:
+            fcntl.flock(self._fd, fcntl.LOCK_EX)
+        except (IOError, OSError):
+            os.close(self._fd)
+            raise
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            fcntl.flock(self._fd, fcntl.LOCK_UN)
+        finally:
+            os.close(self._fd)
+
+@contextmanager
+def access_json(filename: str, factory: Callable[[], Dict], reset: bool=False):
+    path = os.path.join(os.environ.get('GENV_TMPDIR', '/var/tmp/genv'), filename)
+
+    with Umask(0):
+        Path(path).parent.mkdir(parents=True, exist_ok=True, mode=0o777)
+
+        with Flock(f'{path}.lock', 0o666):
+            if os.path.exists(path) and not reset:
+                with open(path) as f:
+                    o = json.load(f)
+            else:
+                o = factory()
+
+            yield o
+
+            with open(path, 'w', opener=lambda path, flags: os.open(path, flags, 0o666)) as f:
+                json.dump(o, f, indent=4)
