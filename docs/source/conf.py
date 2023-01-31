dc4bdd92703165bb501accca183ba522869377a7
Raz Rotenberg <raz.rotenberg@gmail.com>
2023-01-31 10:52:19 +0200

    Setup Google Analytics
    * Using Sphinx extension (https://github.com/sphinx-contrib/googleanalytics)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.5.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinxcontrib.googleanalytics"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -26,3 +26,6 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_extra_path = ["CNAME"]
+
+# Google Analytics
+googleanalytics_id = "G-9BRBX45HVB"
