3bd93587de3bf88d695e0a5154e0d091b4641adc
Raz Rotenberg <raz.rotenberg@gmail.com>
2023-01-31 10:49:32 +0200

    Format conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,24 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'genv'
-copyright = '2022-2023, Run.ai'
-author = 'Raz Rotenberg'
-release = '0.5.0'
+project = "genv"
+copyright = "2022-2023, Run.ai"
+author = "Raz Rotenberg"
+release = "0.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_extra_path = ['CNAME']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_extra_path = ["CNAME"]
