191f812df96d204abbb7a3b5e5beed191e5c539c
Raz Rotenberg <raz.rotenberg@gmail.com>
2023-06-12 11:05:51 +0300

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,19 +5,24 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="genv",
-    version="0.12.0",
-    author="Run:AI",
+    version="1.0.0",
+    author="Run.ai",
     author_email="pypi@run.ai",
-    description="GPU Environment Management",
+    description="GPU environment and cluster management",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/run-ai/genv",
     packages=setuptools.find_packages(),
     package_data={"genv": ["shims/*", "metrics/export/**/*"]},
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: GPU",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Shells",
+        "Topic :: Terminals",
     ],
     entry_points={
         "console_scripts": [
