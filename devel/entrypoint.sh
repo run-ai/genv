6f5e2139e039cbd38f8437f6b4c41a682224a939
Raz Rotenberg <raz.rotenberg@gmail.com>
2024-02-05 13:34:33 +0200

    Supporting not installing Genv in dev containers

--- devel/entrypoint.sh
+++ devel/entrypoint.sh
@@ -2,30 +2,36 @@
 
 set -e
 
-GENV_ROOT=${GENV_ROOT:-$HOME/genv}
+GENV_SRC=${GENV_SRC:-"$HOME/genv"}
+GENV_INSTALL_DEV_SHIMS=${GENV_INSTALL_DEV_SHIMS:-"1"}
+GENV_INSTALL=${GENV_INSTALL:-"1"}
 
-if ! [ -d "$GENV_ROOT" ]; then
-  echo "ERROR: Genv directory not mounted properly"
+if ! [ -d "$GENV_SRC" ]; then
+  echo "ERROR: Genv source directory not mounted properly"
   exit 1
 fi
 
-devel_shims_path="$GENV_ROOT/devel/shims"
+if [[ "$GENV_INSTALL_DEV_SHIMS" = "1" ]]; then
+  devel_shims_path="$GENV_SRC/devel/shims"
 
-shopt -s nullglob
+  shopt -s nullglob
 
-for shim_path in "$devel_shims_path/"*; do
-  shim="${shim_path##$devel_shims_path/}"
+  for shim_path in "$devel_shims_path/"*; do
+    shim="${shim_path##$devel_shims_path/}"
 
-  if ! command -v $shim &> /dev/null
-  then
-    ln -s "$shim_path" "/usr/bin/$shim"
-  fi
-done
+    if ! command -v $shim &> /dev/null
+    then
+      ln -s "$shim_path" "/usr/bin/$shim"
+    fi
+  done
+fi
 
-pip install -e $GENV_ROOT > /dev/null
+if [[ "$GENV_INSTALL" = "1" ]]; then
+  pip install -e $GENV_SRC > /dev/null
 
-cat << EOT >> "$HOME/.bashrc"
+  cat << EOT >> "$HOME/.bashrc"
 eval "\$(genv shell --init)"
 EOT
+fi
 
 ( exec "$@" )
--- docs/source/development/development.rst
+++ docs/source/development/development.rst
@@ -157,6 +157,11 @@ This command runs a container in the background that is named :code:`genv-server
 You can rerun this command as many times as you want to simulate more remote machines.
 Make sure to change the host port each time and also rename the container (or have the container unnamed by omitting the flag :code:`--name` entirely).
 
+.. note::
+
+    By default, Genv is installed in such containers.
+    If you want to skip the installation, for example when testing remote monitoring on machines without Genv, add :code:`-e GENV_INSTALL=0` to the :code:`docker run` command.
+
 You can open a terminal in such a container using a command similar to the following:
 
 .. code-block:: shell
